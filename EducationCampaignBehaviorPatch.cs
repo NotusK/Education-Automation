f827ee4b7d02af48d4373d40aac84a6706a7b4ee
NotusK <adityaar.business@gmail.com>
2024-10-24 06:12:57 +0700

    first commit

---
+++ EducationCampaignBehaviorPatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem.CampaignBehaviors;
+using HarmonyLib;
+
+namespace EducationAutomation
+{
+    [HarmonyPatch(typeof(EducationCampaignBehavior), "RegisterEvents")]
+    public static class EducationCampaignBehaviorPatch
+    {
+        // Prefix method to disable RegisterEvents
+        [HarmonyPrefix]
+        public static bool RegisterEventsDisabled()
+        {
+            // Returning false prevents the original method from being executed
+            return false;
+        }
+    }
+}
---
+++ SubModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+using HarmonyLib;
+
+namespace EducationAutomation
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+            // Initialize Harmony and apply patches
+            var harmony = new Harmony("EducationCampaignBehaviorPatch");
+            harmony.PatchAll();
+        }
+    }
+}
